33f4ea8a1eb7df0b9507891e97eebad7d167086a
Adrian <adrian.hamzah15@gmail.com>
2024-03-01 23:13:15 +0700

    Handled error response

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,14 +31,17 @@ import { setCookie, getCookie, deleteCookie } from "cookies-next";
 import axios, { Axios, AxiosResponse } from "axios";
 import { useState } from "react";
 
+interface ErrorResponse {
+  status: string,
+  data: string
+}
 
 export default function Home() {
-  const [ response, setResponse ] = useState<AxiosResponse<any, any> | undefined>(undefined)
+  const [ response, setResponse ] = useState<AxiosResponse<any, any> | ErrorResponse | undefined>(undefined)
 
-  function onRequest(res: AxiosResponse<any, any>) {
+  function onRequest(res: AxiosResponse<any, any> | ErrorResponse) {
     console.log("--> "+JSON.stringify(res))
-    console.log("---> "+res.data)
-    if (res.data.accessToken && res.data.refreshToken) {
+    if (res.data && res.data.accessToken && res.data.refreshToken) {
       setCookie("ACCESS_TOKEN_USER", res.data.accessToken)
       setCookie("REFRESH_TOKEN_USER", res.data.refreshToken)
     } 
@@ -61,7 +64,7 @@ export default function Home() {
 }
 
 
-function InputURLAndData({ onRequest }: { onRequest: (res: AxiosResponse<any, any>) => void }) {
+function InputURLAndData({ onRequest }: { onRequest: (res: AxiosResponse<any, any> | ErrorResponse) => void }) {
   const formSchema = z.object({
     method: z.string().refine((val: string) => ["GET", "POST", "DELETE", "PATCH"].includes(val), {
       message: "Invalid Method"
@@ -85,14 +88,15 @@ function InputURLAndData({ onRequest }: { onRequest: (res: AxiosResponse<any, an
     defaultValues: {
       method: "",
       requestUrl: "",
-      jsonReqBody: "",
+      jsonReqBody: "{}",
     },
   })
 
   async function onSubmit(values: z.infer<typeof formSchema>) {
-    console.log(values)
-    const dataSent = JSON.parse(values.jsonReqBody)
-    const currentToken = getCookie("ACCESS_TOKEN_USER")
+  try {
+    console.log(values);
+    const dataSent = JSON.parse(values.jsonReqBody);
+    const currentToken = getCookie("ACCESS_TOKEN_USER");
     const requestConfiguration = {
       method: values.method,
       url: values.requestUrl,
@@ -101,83 +105,100 @@ function InputURLAndData({ onRequest }: { onRequest: (res: AxiosResponse<any, an
         'Authorization': `Bearer ${currentToken}`
       },
       data: dataSent
-    }
-    const res = await axios.request(requestConfiguration)
+    };
 
-    if (values.requestUrl === "http://localhost:4000/auth/logout") {
-      deleteCookie("ACCESS_TOKEN_USER")
-      deleteCookie("REFRESH_TOKEN_USER")
+    const res = await axios.request(requestConfiguration);
+
+    console.log("--> ", res);
+
+    if (values.requestUrl === "http://localhost:4000/auth/logout" && (res.status === 200 || res.status === 204)) {
+      deleteCookie("ACCESS_TOKEN_USER");
+      deleteCookie("REFRESH_TOKEN_USER");
     }
 
-    onRequest(res)
+    onRequest(res);
+  } 
+  catch (error: any) {
+    if (error.response) {
+      console.error("Response error:", error.response.data.message);
+      console.error("Status code:", error.response.status);
+      const errorResponse: ErrorResponse = { status: error.response.status, data: error.response.data.message}
+      if (!error.response.data.message) errorResponse.data = error.response.data
+      onRequest(errorResponse)
+    }
   }
-
+}
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col space-y-8 p-3 space-x-1 h-full">
+    <div className="flex flex-col h-full p-3 space-y-2">
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col space-y-8 space-x-1 h-full flex-auto">
 
-      <FormField
-          control={form.control}
-          name="method"
-          render={({ field }) => (
-            <FormItem >
-              <FormLabel>Method</FormLabel>
-              <Select onValueChange={field.onChange} defaultValue={field.value}>
+        <FormField
+            control={form.control}
+            name="method"
+            render={({ field }) => (
+              <FormItem >
+                <FormLabel>Method</FormLabel>
+                <Select onValueChange={field.onChange} defaultValue={field.value}>
+                  <FormControl>
+                    <SelectTrigger>
+                      <SelectValue placeholder="Enter your Request Method" />
+                    </SelectTrigger>
+                  </FormControl>
+                  <SelectContent className="bg-green-100">
+                    <SelectItem value="GET">GET</SelectItem>
+                    <SelectItem value="POST">POST</SelectItem>
+                    <SelectItem value="DELETE">DELETE</SelectItem>
+                    <SelectItem value="PATCH">PATCH</SelectItem>
+                  </SelectContent>
+                </Select>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="requestUrl"
+            render={({ field }) => (
+              <FormItem className="flex-shrink-0">
+                <FormLabel>Request URL</FormLabel>
                 <FormControl>
-                  <SelectTrigger>
-                    <SelectValue placeholder="Enter your Request Method" />
-                  </SelectTrigger>
+                  <Input className="code-input" placeholder="http://localhost:4000/auth/login" {...field} />
                 </FormControl>
-                <SelectContent className="bg-green-100">
-                  <SelectItem value="GET">GET</SelectItem>
-                  <SelectItem value="POST">POST</SelectItem>
-                  <SelectItem value="DELETE">DELETE</SelectItem>
-                  <SelectItem value="PATCH">PATCH</SelectItem>
-                </SelectContent>
-              </Select>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+                <FormMessage />
+              </FormItem>
+            )}
+          />
 
-        <FormField
-          control={form.control}
-          name="requestUrl"
-          render={({ field }) => (
-            <FormItem className="flex-shrink-0">
-              <FormLabel>Request URL</FormLabel>
-              <FormControl>
-                <Input className="code-input" placeholder="http://localhost:4000/auth/login" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+          <FormField
+            control={form.control}
+            name="jsonReqBody"
+            render={({ field }) => (
+              <FormItem className="flex-grow">
+                <FormLabel>Request Body (JSON)</FormLabel>
+                <FormControl className="flex-grow min-h-[0]">
+                  <code className="min-h-[0]">
+                    <Textarea className="flex min-h-[0] h-[90%]" placeholder="{}" {...field} />
+                  </code>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
 
-        <FormField
-          control={form.control}
-          name="jsonReqBody"
-          render={({ field }) => (
-            <FormItem className="flex-grow">
-              <FormLabel>Request Body (JSON)</FormLabel>
-              <FormControl className="flex-grow min-h-[0]">
-                <code className="min-h-[0]">
-                  <Textarea className="flex min-h-[0] h-[90%]" placeholder="{}" {...field} />
-                </code>
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-
-        <Button type="submit">Submit</Button>
-      </form>
-    </Form>
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
+      <Button className="w-full" variant={"destructive"} onClick={async () => await axios.delete("http://localhost:4000/auth/delete-all-token")}>
+        Delete All Token (Only click this when authentication failed)
+      </Button>
+    </div>
   )
 }
 
 
-function ShowResponse({ response }: { response: AxiosResponse<any, any> | undefined}) {
+function ShowResponse({ response }: { response: AxiosResponse<any, any> | ErrorResponse | undefined}) {
   return (
     <div className="h-full flex flex-col space-y-8 p-3">
       <div className="bg-white">
@@ -188,7 +209,7 @@ function ShowResponse({ response }: { response: AxiosResponse<any, any> | undefi
 
       <div className="bg-white overflow-x-auto h-[90%] max-w-full">
         <code className="whitespace-pre-wrap">
-          {JSON.stringify(response?.data, null, 2)}
+          { JSON.stringify(response?.data, null, 2) }
         </code>
       </div>
     </div>
